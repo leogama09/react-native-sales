b4dde85826b593cd4ece5239b6f8a3f501512dc0
Leonardo Gama <leogama09@gmail.com>
2023-05-11 10:33:30 -0300

    inset eslint & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import {Text, View} from "react-native"
+import { Text, View } from 'react-native';
 
 const App = () => {
   return (
     <View>
-        <Text>Testando</Text>
+      <Text>Testando</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
