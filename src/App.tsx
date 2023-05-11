70a6f43a5db81acd321f46522adf7bfde1fbc476
Leonardo Gama <leogama09@gmail.com>
2023-05-11 10:58:51 -0300

    Inserting styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import styled from "styled-components/native"
+
+const TextNew = styled.Text`
+    font-size: 24px;
+    color: blue;
+`
 
 const App = () => {
   return (
     <View>
-      <Text>Testando</Text>
+      <Text style={styles.container}>Testando</Text>
+      <TextNew>Novo teste</TextNew>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        color: 'red'
+    }
+})
 
 export default App;
