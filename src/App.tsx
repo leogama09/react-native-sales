ac5f2f0c08b43138e036bdcee73a0f2a56920d6e
Leonardo Gama <leogama09@gmail.com>
2023-05-16 10:57:06 -0300

    Create modules login 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { SafeAreaView } from 'react-native';
+
 import Login from './modules/login';
 
 const App = () => {
--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -1,11 +1,11 @@
-import {View, Text} from 'react-native'
+import { Text, View } from 'react-native';
 
 const Login = () => {
   return (
     <View>
-        <Text>Login</Text>
+      <Text>Login</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
